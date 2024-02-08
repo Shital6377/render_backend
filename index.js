19befe5e3393854603ee0354b1854ce0b4b0d3ee
Shital6377 <shital.ebiztrait@gmail.com>
2024-02-08 17:07:09 +0530

    changes for connection function

--- index.js
+++ index.js
@@ -13,41 +13,52 @@ app.use(cors(
 ));
 app.use(express.json())
 
-const dbUrl =  "mongodb+srv://sarmistha1:sarmistha@cluster0.npn1qmh.mongodb.net/store";
+const dbUrl = "mongodb+srv://sarmistha1:sarmistha@cluster0.npn1qmh.mongodb.net/store";
 
 const connectionParams = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 };
 
-mongoose.connect(dbUrl, connectionParams);
 
+
+const connectDB = () => {
+
+    mongoose.connect(dbUrl, connectionParams).then((res) => {
+        console.log(`Successfully connected to MongoDB 👍`);
+    }).catch((error) => {
+        console.error(`Error: ${error.message}`);
+    })
+    
+};
+
+connectDB();
 
 app.get("/", (req, res) => {
     return res.json("Hello");
 })
 
-app.post('/login',(req,res)=>{
-    const{email,password}=req.body;
-    EmployeeModel.findOne({email: email})
-    .then(user=>{
-        if(user){
-            if(user.password === password){
-                res.json('success')
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    EmployeeModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json('success')
+                }
+                else {
+                    res.json('The password is incorrect')
+                }
+            } else {
+                res.json('No record existed')
             }
-            else{
-                res.json('The password is incorrect')
-            }
-        } else{
-            res.json('No record existed')
-        }
-    })
+        })
 })
 
-app.post('/register',(req,res)=>{
+app.post('/register', (req, res) => {
     EmployeeModel.create(req.body)
-    .then(employee => res.json(employee))
-    .catch(err => res.json(err))
+        .then(employee => res.json(employee))
+        .catch(err => res.json(err))
 })
 
 
